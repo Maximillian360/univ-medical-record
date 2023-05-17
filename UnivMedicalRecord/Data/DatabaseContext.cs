7ddae9efae25ad2afa886f5fce738afc4b7fa621
David <erosval126@gmail.com>
2023-05-17 20:11:22 +0800

    Added Update and Add record functions

--- UnivMedicalRecord/Data/DatabaseContext.cs
+++ UnivMedicalRecord/Data/DatabaseContext.cs
@@ -41,11 +41,35 @@ public class DatabaseContext: DbContext
         return users;
     }
     
+    public  IEnumerable<FamilyInfo> GetFamilyInfo()
+    {
+        var family = FamilyInfos.ToList();
+        return family;
+    }
+    
+    public  IEnumerable<Personal> GetPersonals()
+    {
+        var personal = Personals.ToList();
+        return personal;
+    }
+    
+    public  IEnumerable<Illness> GetIllness()
+    {
+        var illness = Illnesses.ToList();
+        return illness;
+    }
+    public  IEnumerable<Allergy> GetAllergies()
+    {
+        var allergy = Allergies.ToList();
+        return allergy;
+    }
+    
     public User? GetUser(int id)
     {
         return GetUsers().FirstOrDefault(x => x.Id == id);
     }
-
+    
+    
     public bool HasSuperAdmin()
     {
         return AdminRoles.Any(x => x.Position == Position.SuperAdmin);
@@ -54,6 +78,24 @@ public class DatabaseContext: DbContext
     public bool AddUser(User user)
     {
         Users.Add(user);
+        var changesSaved = SaveChanges();
+        return changesSaved > 0;
+    }
+    public bool AddRecord(Personal personal, Allergy allergy, FamilyInfo familyInfo, Illness illness)
+    {
+        Personals.Add(personal);
+        FamilyInfos.Add(familyInfo);
+        Allergies.Add(allergy);
+        Illnesses.Add(illness);
+        var changesSaved = SaveChanges();
+        return changesSaved > 0;
+    }
+    public bool UpdateRecord(Personal personal, Allergy allergy, FamilyInfo familyInfo, Illness illness)
+    {
+        Personals.Update(personal);
+        FamilyInfos.Update(familyInfo);
+        Allergies.Update(allergy);
+        Illnesses.Update(illness);
         var changesSaved = SaveChanges();
         return changesSaved > 0;
     }
